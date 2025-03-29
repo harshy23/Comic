b81c433c106b15eed8122dff4949c0f7d73a6a76
harshy23 <0harshmishra0@gmail.com>
2025-03-29 14:09:24 +0530

    start

--- shopy/settings.py
+++ shopy/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'img/'
 MEDIA_ROOT = BASE_DIR/"media"
 
